7c1f12aec99d2e23c4bbf536d53e9706f8977ca6
Руслан Аитов <r_aitov@mail.ru>
2024-01-24 14:55:16 +0300

    task5

--- task5/rand.py
+++ task5/rand.py
@@ -1,2 +1,18 @@
 def f(n):
     s1 = bin(n)[2:]
+    if s1.count('1') > s1.count('0'):
+        s2 = s1 + '0'
+    else:
+        s2 = s1 + '1'
+    if s2.count('1') > s2.count('0'):
+        s3 = s2 + '0'
+    else:
+        s3 = s2 + '1'
+    return int(s3, 2)
+
+
+res = []
+for i in range(2, 1000):
+    if f(i) < 57:
+        res.append(f(i))
+print(max(res))
