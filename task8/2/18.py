b84361cc4a8ac40532364e493630d351e9257705
r_aitov <r_aitov@mail.ru>
2024-04-10 20:38:47 +0300

    task26

--- task8/2/18.py
+++ task8/2/18.py
@@ -1,9 +1,9 @@
 from itertools import permutations
 
-words = [''.join(word) for word in permutations('ТИМУР', r=4)]
+words = [''.join(word) for word in permutations('перевыборы', r=10)]
 a = set()
 for word in words:
-    t = word.replace('Т', '1').replace('И', '0').replace('М', '1').replace('У', '0').replace('Р', '1')
+    t = word.replace('п', '1').replace('е', '0').replace('р', '1').replace('в', '1').replace('ы', '0').replace('б', '1').replace('о', '0')
     if '00' not in t and '11' not in t:
         a.add(word)
 print(len(a))
